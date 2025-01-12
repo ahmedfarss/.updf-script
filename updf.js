875147698798b73c8ee9625551a76589c840daa7
ahmedfarss <qwertvgvg758@gmail.com>
2025-01-12 23:09:21 +0300

    Delete updf.js

--- updf.js
+++
@@ -1,9 +0,0 @@
-let body = $response.body;
-let obj = JSON.parse(body);
-
-// فتح الميزات المدفوعة
-obj.data.isPremium = true;
-obj.data.subscriptionStatus = "active";
-
-body = JSON.stringify(obj);
-$done({body});
